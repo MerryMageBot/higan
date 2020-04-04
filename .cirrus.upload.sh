2d27fa5fdffc20276d55ad60d289bc9709334346
MerryMage <MerryMage@users.noreply.github.com>
2020-04-04 16:27:06 +0100

    ..

---
+++ .cirrus.upload.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ "$CIRRUS_RELEASE" == "" ]]; then
+  echo "Not a release. No need to deploy!"
+  exit 0
+fi
+
+if [[ "$GITHUB_TOKEN" == "" ]]; then
+  echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
+  exit 1
+fi
+
+echo "Uploading..."
+name="higan-$CIRRUS_RELEASE-$CIRRUS_OS.zip"
+curl -X POST \
+    --data-binary "higan-nightly.zip" \
+    --header "Authorization: token $GITHUB_TOKEN" \
+    --header "Content-Type: application/octet-stream" \
+    "https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
